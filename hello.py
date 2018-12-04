e320ea06b951cebf2986d779c38652119314d1fc
kamrul4188 <kamrul4188@gmail.com>
2018-12-05 00:50:13 +0800

    Added file math_operator and hello

---
+++ hello.py
@@ -0,0 +1,23 @@
+#This is basic python program. here we will use print finction for print a statement in various format
+
+#print welcome massage
+print ("Welcome to basic python\n")
+
+#Declare variable and print
+text_to_print = "hello world"
+print (text_to_print)
+
+
+#Declare multiple variable and print
+a = 2
+b = 3.5
+c = "kamrul"
+print(a, b, c)
+
+#Adding two string and print
+first_name = "Bangla"
+last_name = "desh"
+
+full_name = first_name + last_name
+
+print(full_name)
---
+++ math_operator.py
@@ -0,0 +1,32 @@
+#Basic math operator "+,-,/, %, //, **"
+
+print("\n----------------------------------------\n")
+
+a = input("Please enter  value of a : ")
+b = input("Please enter value of b : ")
+
+print("\n----------------------------------------\n")
+
+print "your input a =",a, "and b =",b
+
+print "Result of basic math operation"
+
+c = a + b
+print "a + b =", c
+
+c = a - b
+print "a - b =", c
+
+c = a / b
+print "a / b =", c
+
+c = a % b
+print "a % b =", c
+
+c = a // b
+print "a // b =", c
+
+c = a**b
+print "a ** b =", c
+
+print("\n----------------------------------------\n")
