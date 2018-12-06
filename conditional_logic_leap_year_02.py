9cb8ee0a620c76d3599ac0ea11b605e2a5b977a6
kamrul4188 <kamrul4188@gmail.com>
2018-12-06 18:30:50 +0800

    New file added

---
+++ conditional_logic_leap_year_02.py
@@ -0,0 +1,13 @@
+#Conditional logic leap year 02
+
+year = input("please enter year: ")
+
+if year %400 == 0:
+    print year, "is a leap year"
+elif year %100 == 0:
+    print year, "is not a leap year"
+elif year %4 == 0:
+    print year, "is a leap year"
+else:
+    print year, "is not a leap year"
+    
