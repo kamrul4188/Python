d249b0c36efecd6fc5925749c88671f38cf5a6b4
kamrul4188 <kamrul4188@gmail.com>
2018-12-06 21:42:16 +0800

    new file added

---
+++ loop_01.py
@@ -0,0 +1,12 @@
+#introduction to for loop
+
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(2)
+
+for i in range (4):
+    turtle.forward(100)
+    turtle.left(90)
+
+turtle.exitonclick()
---
+++ loop_02.py
@@ -0,0 +1,7 @@
+result = 0
+
+for i in range(50):
+    result = result+1
+    print(result)
+
+    
