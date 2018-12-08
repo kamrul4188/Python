3778ca4853175f279d56990f8bcc6cf751b34aef
kamrul4188 <kamrul4188@gmail.com>
2018-12-08 16:14:54 +0800

    new file added

---
+++ function_01.py
@@ -0,0 +1,11 @@
+def add(n1, n2):
+    return n1 + n2
+
+while True:
+    n = input ("Please enter integer value for n : ")
+    n = int(n)
+    m = input ("Please enter integer value for m : ")
+    m = int(m)
+
+    result = add(n, m)
+    print(result)
