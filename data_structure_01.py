8fcf492bd7e4d108b35974c4bad889e066510f47
kamrul4188 <kamrul4188@gmail.com>
2018-12-10 19:06:07 +0800

    updated

--- data_structure_01.py
+++ data_structure_01.py
@@ -2,12 +2,12 @@ li = []
 
 print ("\nWelcome ! This program we are going to create 'List'")
 while True:
-    print("===========================================================")
+    print("---------------------------------------------------------------------")
     print("List = ", li)
+    print("---------------------------------------------------------------------")
 
-
-
-    number = input ("Please enter your preferancnce [add/remove/sort/reverse/exit] : ")
+    number = input ("Please enter your preferancnce add/remove/sort/reverse/exit/help : ")
+    number = number.lower()
 
     if number == "add":
         name = input("Enter a name you want to add: ")
@@ -15,10 +15,15 @@ while True:
     elif number == "rm":
         name = input ("Enter a name you want to remove: ")
         li.remove(name) #Remove from the list
+    elif number == "help":
+        print("add = adding new name ")
+        print("remove = remove name from the list")
+        print("sort = sort a to z the list")
+        print("reverse = reverse order of list")
+        print("exit = terminate the Program")
     elif number == "exit":
-        print ("Program terminated")
+        print ("Program terminated...")
         break
     else:
-        print("add = adding new name ")
-        print("rm = remove name from the list")
-        print("exit = terminate the Program")
+        print("your input not match! Program terminated...")
+        break
