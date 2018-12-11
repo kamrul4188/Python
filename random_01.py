f146d798e0439a080b2dbea08aa6bedc8ec93e3b
kamrul4188 <kamrul4188@gmail.com>
2018-12-11 21:45:16 +0700

    update

--- random_01.py
+++ random_01.py
@@ -1,10 +1,20 @@
 import turtle
 import random
 
+turtle.penup()
+
+#List of colors
+colors = ["red", "green", "blue", "yellow", "orange", "black", "purple"]
+
 for i in range(20):
+    #innitialize range of randoem number
     x = random.randint(-200, 200)
     y = random.randint(-200, 200)
+    #set the random position
     turtle.setposition(x,y)
-    turtle.dot()
+    #set random colors
+    i = random.randint(0, len(colors)-1)
 
-turtle.clickonexit()
+    turtle.dot(colors[i])
+
+turtle.exitonclick()
